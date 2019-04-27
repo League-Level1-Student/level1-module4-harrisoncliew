771d7678fb87ed3fec8c026ac3bb6967cf0bb94a
edemarest <47263131+edemarest@users.noreply.github.com>
2019-04-26 17:00:27 -0700

    TypingTutor, BookOfIllusions

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,4 +1,77 @@
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Date;
+import java.util.Random;
+import javax.swing.*;
+public class TypingTutor implements KeyListener {
+	static char currentLetter;
+	static int n = 0;
+public static void main(String[] args) {
+	
+	currentLetter = randomletter();
+	TypingTutor tt = new TypingTutor();
+	tt.setup();
+	
+}
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	void setup() {
+		frame.add(panel);
+		panel.add(label);
+		label.setText(currentLetter+"");
+		label.setFont(label.getFont().deriveFont(40.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		frame.addKeyListener(this);
+		frame.setTitle("Type or Die");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.pack();
+	}
+	static char randomletter() {
+		Random r = new Random();
+		return (char) (r.nextInt(26)+'a');
+	}
+	Date timeAtStart = new Date();
 
-public class TypingTutor {
-
+	private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+	      Date timeAtEnd = new Date();
+	      long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+	      long gameInSeconds = (gameDuration / 1000) % 60;
+	      double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+	      int charactersPerMinute = (int) (charactersPerSecond * 60);
+	      JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("You typed: "+ e.getKeyChar());
+		if(e.getKeyChar()==currentLetter) {
+			System.out.println("Correct");
+			panel.setBackground(Color.GREEN);
+			
+			
+		}
+		else {
+			panel.setBackground(Color.RED);
+		}
+		n++;
+		if(n==50) {
+			showTypingSpeed(n);
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		currentLetter = randomletter();
+		label.setText(" ");
+		label.setText(currentLetter+"");
+		
+	}
 }
--- src/book_of_illusions/BookOfIllusions.java
+++ src/book_of_illusions/BookOfIllusions.java
@@ -1,5 +1,6 @@
 package book_of_illusions;
-
+import javax.swing.*;
+import javax.swing.JOptionPane;
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
@@ -18,27 +19,48 @@ import javax.swing.JLabel;
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
+	JFrame frame = new JFrame();
+	JLabel label;
+	JLabel label2;
+	String image1 = "illusion.jpeg";
+	String image2 = "pika.jpg";
 
 	public void run() {
 		// 2. make the frame visible
+		frame.setVisible(true);
 		// 3. set the size of the frame
+		frame.setSize(800, 1000);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		label = loadImageFromComputer(image2);
+		
 		// 8. add your JLabel to the frame
+		frame.add(label);
+		
 		// 9. call the pack() method on the frame
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
-
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked!");
 		// 12. remove everything from the frame that was added earlier
+		frame.remove(label);
 		// 13. load a new image like before (this is more than one line of code)
+		label = loadImageFromComputer(image1);
+		frame.add(label);
 		// 14. pack the frame
+		frame.pack();
+		JOptionPane.showMessageDialog(null, "Hello!");
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
+	
 
 	/*
 	 * To use this method, the image must be placed in your Eclipse project under "default package".
